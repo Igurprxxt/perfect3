9ca0913fd313832288b30d8468ad9f140d5b36a1
Gurpreet Singh <103951591+Igurprxxt@users.noreply.github.com>
2025-07-25 08:04:04 +0530

    updted

--- pages/index.js
+++ pages/index.js
@@ -247,7 +247,7 @@ const Index = () => {
                           <TextArea
                             placeholder="Write Message"
                             className={
-                              "!text-black flex w-full rounded-md border border-input bg-background text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+                              "text-black flex w-full rounded-md border border-input bg-background ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
                             }
                             rows={5}
                             {...register("message", {
